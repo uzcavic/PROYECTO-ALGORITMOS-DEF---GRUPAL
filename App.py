724bfcc8700966cf468a60f57a2505edd3ea2edc
Andres Teixeira <andres.teixeira@correo.unimet.edu.ve>
2024-08-10 19:13:34 -0400

    menu cambios

--- App.py
+++ App.py
@@ -24,16 +24,11 @@ class APP:
     def start(self):
 
         self.crear_pelicula() #completada
-        for pelicula in self.pelicula_lista:
-            pelicula.showPelicula()
-        
         self.crear_especies() #completada
-        for especie in self.especie_lista:
-            especie.showEspecie()
-
         self.crear_planeta() #en curso
-
+        
         self.crear_personaje() #en curso
+
         self.relacionar_personajes_con_planetas()
         for  planeta in self.planeta_lista:
             planeta.showPlaneta()
@@ -42,7 +37,6 @@ class APP:
         print("NAVES")
         for nave in self.nave_lista:
             nave.showNave()
-
         print("Vehiculos")
         self.crear_vehiculos() #en curso
         for vehiculo in self.vehiculos_lista:
@@ -50,6 +44,65 @@ class APP:
         self.relacionar_vehiculos_y_naves_con_personajes()
         for personaje in self.personaje_lista:
             personaje.showPersonaje()
+        while True:
+            menu= input("""Bienvenido viajero, indiqueme que decea saber del universo de star wars
+    1. lista de peliculas
+    2. lista de especies
+    3. lista de planetas
+    4. buscar personaje
+    5. graficos de la poblacion de los planetas
+    6. Graficos diferenciadores de las naves
+    7. Estadisticas basicas sobre valores de la nave
+    8. mision                   
+    9. Terminar el programa
+    ---> """).strip()
+            if menu =="2":
+                index=1
+                for especie in self.especie_lista:
+                    especie:Especie
+                    print(f"Pelicula N°{index}\n")
+                    especie.showEspecie()
+                    index+=1
+            elif menu =="1":
+                index=1
+                for pelicula in self.pelicula_lista:
+                    pelicula:Pelicula
+                    print(f"Pelicula N°{index}\n")
+                    pelicula.showPelicula()
+                    index+=1 
+            elif menu =="3":
+                index=1
+                for planeta in self.planeta_lista:
+                    planeta:Planeta
+                    print(f"Pelicula N°{index}\n")
+                    planeta.showPlaneta()
+                    index+=1                 
+            elif menu =="5":
+                self.grafica_planetas()
+            elif menu =="6":
+                self.Grafica_nave()  
+            elif menu =="7":
+                 self.Estadistica_nave()    
+            elif menu =="9":
+                print("""Gracias y que la fuerza te acompañe
+                      
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⠀⢀⣤⣤⣤⣤⣤⣤⣤⠀⠀⠀⠀⢠⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣼⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣟⠛⠛⠛⠛⠛⣿⣿⣿⣿⡟⠛⠛⠛⠛⠛⢠⣿⣿⣿⣿⠿⣿⣿⣿⣿⡀⠀⠀⢸⣿⣿⣿⣿⡏⠉⠉⢉⣿⣿⣿⣿⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣷⡀⠀⠀⠀⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⣾⣿⣿⣿⡟⠀⢻⣿⣿⣿⣧⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀
+⣤⣤⣤⣤⣤⣤⣤⣤⣤⣬⣿⣿⣿⣿⣿⣷⠀⠀⠀⣿⣿⣿⣿⡇⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣤⣤⣤⣤⣤⣤
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⣿⣿⣿⣿⡇⠀⠀⠀⢀⣿⣿⣿⣿⡿⠿⠿⠿⠿⣿⣿⣿⣿⡀⢸⣿⣿⣿⣿⡇⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠟⠛⠁⠀⠀⠀⠀⠿⠿⠿⠿⠇⠀⠀⠀⠸⠿⠿⠿⠟⠀⠀⠀⠀⠀⠻⠿⠿⠿⠇⠸⠿⠿⠿⠿⠇⠀⠀⠙⠛⠿⠿⠿⠿⠿⠿⠿⠿
+⢻⣿⣿⣿⣿⡄⢠⣿⣿⣿⣿⣿⡆⠀⣾⣿⣿⣿⡟⠀⢀⣾⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣦⣄⠀⠀⠀⠀⣠⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿
+⠈⣿⣿⣿⣿⣷⣼⣿⣿⣿⣿⣿⣿⣸⣿⣿⣿⣿⠁⠀⣼⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⣿⣿⣿⣿⣿⠿⠿⢿⣿⣿⣿⣿⣇⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⢠⣿⣿⣿⣿⠏⢿⣿⣿⣿⣇⠀⠀⠀⣿⣿⣿⣿⣿⣀⣀⣀⣼⣿⣿⣿⡟⠀⠀⢹⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀
+⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⣾⣿⣿⣿⣟⣀⣸⣿⣿⣿⣿⡀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠙⢿⣿⣿⣿⣿⣧⠀⠀⠀⠀
+⠀⠀⠘⣿⣿⣿⣿⣿⣿⠋⣿⣿⣿⣿⣿⣿⠇⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣶⣶⣾⣿⣿⣿⣿⣿⠇⠀⠀⠀
+⠀⠀⠀⢻⣿⣿⣿⣿⡏⠀⢹⣿⣿⣿⣿⡟⠀⢀⣿⣿⣿⣿⡟⠛⠛⠛⠛⣿⣿⣿⣿⡆⠀⣿⣿⣿⣿⣿⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀
+⠀⠀⠀⠈⠉⠉⠉⠉⠀⠀⠀⠉⠉⠉⠉⠁⠀⠈⠉⠉⠉⠉⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠀⠉⠉⠉⠉⠉⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀                      """)
+                break    
+            else:
+                print("Lo lamento no se que quisiste poner") 
 
 
     def grafica_planetas(self):            
